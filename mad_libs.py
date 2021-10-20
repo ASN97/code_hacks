abb470b514b80b742aac5e11acd51a638e743234
asn_97 <30555819+ASN97@users.noreply.github.com>
2021-10-21 00:36:06 +0530

    Update mad_libs.py

--- mad_libs.py
+++ mad_libs.py
@@ -1,59 +1,25 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;}
-{\colortbl;\red255\green255\blue255;\red238\green252\blue122;\red30\green31\blue41;\red246\green246\blue239;
-\red174\green122\blue247;\red252\green93\blue186;}
-{\*\expandedcolortbl;;\cssrgb\c94510\c98039\c54902;\cssrgb\c15686\c16471\c21176;\cssrgb\c97255\c97255\c94902;
-\cssrgb\c74118\c57647\c97647;\cssrgb\c100000\c47451\c77647;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-""" Mad Libs Generator\cf4 \cb1 \
-\cf2 \cb3 ----------------------------------------\cf4 \cb1 \
-\cf2 \cb3 """\cf4 \cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \cb3 //Loop back to this point once code finishes\cb1 \
-\cb3 loop = \cf5 1\cf4 \cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\b \cf6 \cb3 while
-\f0\b0 \cf4  (loop < \cf5 10\cf4 ):\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf4 \cb3 // All the questions that the program asks the user\cb1 \
-\cb3  \'a0\'a0\'a0noun = input(\cf2 "Choose a noun: "\cf4 )\cb1 \
-\cb3 \'a0\'a0\'a0\'a0p_noun = input(\cf2 "Choose a plural noun: "\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0noun2 = input(\cf2 "Choose a noun: "\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0place = input(\cf2 "Name a place: "\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0adjective = input(\cf2 "Choose an adjective (Describing word): "\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0noun3 = input(\cf2 "Choose a noun: "\cf4 )\cb1 \
-\cb3 // Displays the story based on the users input\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "------------------------------------------"\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "Be kind to your"\cf4 ,noun,\cf2 "- footed"\cf4 , p_noun)\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "For a duck may be somebody's"\cf4 , noun2,\cf2 ","\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "Be kind to your"\cf4 ,p_noun,\cf2 "in"\cf4 ,place)\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "Where the weather is always"\cf4 ,adjective,\cf2 "."\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  ()\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "You may think that is this the"\cf4 ,noun3,\cf2 ","\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "Well it is."\cf4 )\cb1 \
-\cb3  \'a0\'a0\'a0
-\f1\b \cf6 print
-\f0\b0 \cf4  (\cf2 "------------------------------------------"\cf4 )\cb1 \
-\cb3 // Loop back to \cf2 "loop = 1"\cf4 \cb1 \
-\cb3 \'a0\'a0\'a0\'a0loop = loop + \cf5 1}
+""" Mad Libs Generator
+----------------------------------------
+"""
+//Loop back to this point once code finishes
+loop = 1
+while (loop < 10):
+// All the questions that the program asks the user
+    noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
+// Displays the story based on the users input
+    print ("------------------------------------------")
+    print ("Be kind to your",noun,"- footed", p_noun)
+    print ("For a duck may be somebody's", noun2,",")
+    print ("Be kind to your",p_noun,"in",place)
+    print ("Where the weather is always",adjective,".")
+    print ()
+    print ("You may think that is this the",noun3,",")
+    print ("Well it is.")
+    print ("------------------------------------------")
+// Loop back to "loop = 1"
+    loop = loop + 1
